5cd9c80b1e18e0ec29247e9fe3e7d1b68fbbf86b
Raj Kumar Raj <143611911+Raj-Kumar-Raj@users.noreply.github.com>
2026-05-25 12:12:29 +0530

    Refactor HomeComponent for improved readability

--- fronted/src/pages/home.jsx
+++ fronted/src/pages/home.jsx
@@ -26,7 +26,7 @@ function HomeComponent() {
 
                 <div style={{ display: "flex", alignItems: "center" }}>
 
-                    <h2>Call Sphere</h2>
+                    <h2>CallSphere</h2>
                 </div>
 
                 <div style={{ display: "flex", alignItems: "center" }}>
